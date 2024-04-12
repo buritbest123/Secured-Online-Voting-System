1eaefcadb2cbbb0f13722dd9168ae9de1d59f03d
Ramita Deeprom <62588305+RamitaDeeprom@users.noreply.github.com>
2024-04-12 20:53:50 +0700

    Change vote buton size

--- VotingPage.py
+++ VotingPage.py
@@ -37,19 +37,19 @@ def votingPg(root,frame1,client_socket):
 
     vote = StringVar(frame1,"-1")
 
-    Radiobutton(frame1, text = "EarthPony\nMeadowbrook", fg=fontColor, bg=buttonColor, variable = vote, value = "EarthPony", indicator = 0, height = 4, width=15, command = lambda: voteCast(root,frame1,"EarthPony",client_socket)).grid(row = 2,column = 1)
+    Radiobutton(frame1, text = "EarthPony\nMeadowbrook", fg=fontColor, bg=buttonColor, variable = vote, value = "EarthPony", indicator = 0, height = 2, width=15, command = lambda: voteCast(root,frame1,"EarthPony",client_socket)).grid(row = 2,column = 1)
     electionCandidate1 = ImageTk.PhotoImage((Image.open("img/EarthPony.png")).resize((45,45),Image.LANCZOS))
     EarthPonyImg = Label(frame1, image=electionCandidate1, bg=bgColor).grid(row = 2,column = 0)
 
-    Radiobutton(frame1, text = "Pegasus\nSkydancer", fg=fontColor, bg=buttonColor, variable = vote, value = "Pegasus", indicator = 0, height = 4, width=15, command = lambda: voteCast(root,frame1,"Pegasus",client_socket)).grid(row = 3,column = 1)
+    Radiobutton(frame1, text = "Pegasus\nSkydancer", fg=fontColor, bg=buttonColor, variable = vote, value = "Pegasus", indicator = 0, height = 2, width=15, command = lambda: voteCast(root,frame1,"Pegasus",client_socket)).grid(row = 3,column = 1)
     electionCandidate2 = ImageTk.PhotoImage((Image.open("img/Pegasus.png")).resize((35,48),Image.LANCZOS))
     PegasusImg = Label(frame1, image=electionCandidate2, bg=bgColor).grid(row = 3,column = 0)
 
-    Radiobutton(frame1, text = "Unicorn\nStarwhisper", fg=fontColor, bg=buttonColor, variable = vote, value = "Unicorn", indicator = 0, height = 4, width=15, command = lambda: voteCast(root,frame1,"Unicorn",client_socket) ).grid(row = 4,column = 1)
+    Radiobutton(frame1, text = "Unicorn\nStarwhisper", fg=fontColor, bg=buttonColor, variable = vote, value = "Unicorn", indicator = 0, height = 2, width=15, command = lambda: voteCast(root,frame1,"Unicorn",client_socket) ).grid(row = 4,column = 1)
     electionCandidate3 = ImageTk.PhotoImage((Image.open("img/Unicorn.png")).resize((55,40),Image.LANCZOS))
     UnicornImg = Label(frame1, image=electionCandidate3, bg=bgColor).grid(row = 4,column = 0)
 
-    Radiobutton(frame1, text = "Alicorn\nCelestiaDream", fg=fontColor, bg=buttonColor, variable = vote, value = "Alicorn", indicator = 0, height = 4, width=15, command = lambda: voteCast(root,frame1,"Alicorn",client_socket)).grid(row = 5,column = 1)
+    Radiobutton(frame1, text = "Alicorn\nCelestiaDream", fg=fontColor, bg=buttonColor, variable = vote, value = "Alicorn", indicator = 0, height = 2, width=15, command = lambda: voteCast(root,frame1,"Alicorn",client_socket)).grid(row = 5,column = 1)
     electionCandidate4 = ImageTk.PhotoImage((Image.open("img/Alicorn.png")).resize((50,45),Image.LANCZOS))
     AlicornImg = Label(frame1, image=electionCandidate4, bg=bgColor).grid(row = 5,column = 0)
 
